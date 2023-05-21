405ae6a57086067e46f71c4cf0e30207a40a5ea9
m'sahel mohamed habib <48933499+mohamedhabibmsahel@users.noreply.github.com>
2023-05-21 13:16:08 +0100

    edit gitignore

--- my_travel_app/lib/main.dart
+++ my_travel_app/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:get/get_navigation/src/routes/transitions_type.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,21 +13,21 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return GetMaterialApp(
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        textTheme: TextTheme(
+          headline6: GoogleFonts.exo2(
+            color: Colors.white,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Billion Dollar App',
+      debugShowCheckedModeBanner: false,
+      defaultTransition: Transition.rightToLeft,
+      getPages: Routes.route,
+      initialRoute:"/homePageView" ,
     );
   }
 }
