d571cd0dd33a89b6781e68bfa003dae43404a752
m'sahel mohamed habib <48933499+mohamedhabibmsahel@users.noreply.github.com>
2023-07-16 18:25:03 +0100

    recuperation Password

--- my_travel_app/lib/main.dart
+++ my_travel_app/lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:my_travel_app/controllers/login_controller.dart';
+import 'package:my_travel_app/controllers/recuperationPassword_controller.dart';
 import 'package:my_travel_app/controllers/verification_code_controller.dart';
 import 'package:my_travel_app/views/login_View.dart';
 import 'package:my_travel_app/views/on_boarding_screen.dart';
+import 'package:my_travel_app/views/recuperationPassword_View.dart';
 import 'package:my_travel_app/views/verificationCode_view.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -18,11 +20,12 @@ Future<void> main() async {
 
   runApp(GetMaterialApp(
     initialBinding: InitialBindings(),
-    home: token != null && token ?  LoginView() :  OnBoardingScreen(),
+    home: token != null && token ?  const LoginView() :  OnBoardingScreen(),
     getPages: [
       GetPage(name: '/onBoardingScreen', page: () => OnBoardingScreen(),binding: BindingsBuilder.put(() => OnBoardingController()) ),
       GetPage(name: '/LoginView', page: () =>  const LoginView(),binding: BindingsBuilder.put(() => LoginController())),
       GetPage(name: '/VerificationCodeView', page: () =>  VerificationCodeView(),binding: BindingsBuilder.put(() => VerificationCodeController())),
+      GetPage(name: '/RecuperationPasswordView', page: () =>  RecuperationPasswordView(),binding: BindingsBuilder.put(() => RecuperationPasswordController())),
     ],
   ));
 }
@@ -33,5 +36,7 @@ class InitialBindings implements Bindings {
     Get.put<OnBoardingController>(OnBoardingController(), permanent: true);
     Get.put<LoginController>(LoginController(), permanent: true);
     Get.put<VerificationCodeController>(VerificationCodeController(), permanent: true);
+    Get.put<RecuperationPasswordController>(RecuperationPasswordController(), permanent: true);
+
   }
 }
